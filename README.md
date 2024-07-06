a2c536a025d4c59b12b0b485312850965c500e8f
Georgy Klochkov <klochkovg@gmail.com>
2024-07-06 23:14:40 +0200

    scaffold UI

--- README.md
+++ README.md
@@ -27,3 +27,17 @@ You can check out the [create-t3-app GitHub repository](https://github.com/t3-os
 ## How do I deploy this?
 
 Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/vercel), [Netlify](https://create.t3.gg/en/deployment/netlify) and [Docker](https://create.t3.gg/en/deployment/docker) for more information.
+
+## TODO
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
+- [ ] Actually set up a database (vercel postgres)
+- [ ] Attach database to UI
+- [ ] Add authentication (with clerk)
+- [ ] Add image upload
+- [ ] Erro management (with Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete button (with server acution
+- [ ] analytics (posthog)
+- [ ] ratelimiting (upstash)
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/c6ecb3b1-9b7d-46ca-b171-16b5e1cbb428-po818i.jpg",
+  "https://utfs.io/f/db6947f8-ac61-4dc4-9dd6-54bf4e7bc8e7-9w7z9x.jpg",
+  "https://utfs.io/f/9624e7a2-c887-4774-a2cb-5cf192709af0-dtsdi5.jpg",
+  "https://utfs.io/f/0c71db51-3ed6-4400-9d79-767f1ed5ab8b-j13dcb.jpg"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello gallery in progress!
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+          <img src={image.url}/>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
